e8f0d2eda15a2f5298dfaa37c208b826cc44a533
Joachim Seminck <joachim@seminck.be>
2017-10-15 19:51:11 +0300

    Refactor keyCode into code

--- public/js/KeyboardState.js
+++ public/js/KeyboardState.js
@@ -9,23 +9,23 @@ export default class KeyboardState {
         this.keyMap = new Map()
     }
 
-    addMapping(keyCode, callback) {
-        this.keyMap.set(keyCode, callback)
+    addMapping(code, callback) {
+        this.keyMap.set(code, callback)
     }
 
     handleEvent(event) {
-        const { keyCode } = event
+        const { code } = event
 
-        if (!this.keyMap.has(keyCode)) return
+        if (!this.keyMap.has(code)) return
 
         event.preventDefault()
         const keyState = event.type === 'keydown' ? PRESSED : RELEASED
 
-        if (this.keyStates.get(keyCode) !== keyState) {
-            if (this.keyMap.has(keyCode))
-                this.keyMap.get(keyCode)(keyState, event)
+        if (this.keyStates.get(code) !== keyState) {
+            if (this.keyMap.has(code))
+                this.keyMap.get(code)(keyState, event)
 
-            this.keyStates.set(keyCode, keyState)
+            this.keyStates.set(code, keyState)
         }
     }
 
--- public/js/input.js
+++ public/js/input.js
@@ -1,26 +1,22 @@
 import KeyboardState from './KeyboardState.js'
 
-const SPACE = 32
-const RIGHT_ARROW = 39
-const LEFT_ARROW = 37
-
 // eslint-disable-next-line
 export function setupInput(mario) {
     const keyboardState = new KeyboardState()
 
-    keyboardState.addMapping(SPACE, state => {
+    keyboardState.addMapping('Space', state => {
         if (state)
             mario.jump.start()
         else
             mario.jump.abort()
     })
 
-    keyboardState.addMapping(RIGHT_ARROW, state => {
+    keyboardState.addMapping('ArrowRight', state => {
         mario.go.dir = state;
     })
 
 
-    keyboardState.addMapping(LEFT_ARROW, state => {
+    keyboardState.addMapping('ArrowLeft', state => {
         mario.go.dir = -state;
     })
 
