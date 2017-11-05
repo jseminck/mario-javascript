d410ef2b32d19e65df415aae53f7ecb46bddf15b
Joachim Seminck <joachim@seminck.be>
2017-11-05 21:31:54 +0200

    Beautify level json

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -28,17 +28,32 @@ export function loadLevel(name) {
 }
 
 function createTiles(level, backgrounds) {
-    backgrounds.forEach(background => {
-        background.ranges.forEach(([xStart, xLen, yStart, yLen]) => {
-            const xEnd = xStart + xLen;
-            const yEnd = yStart + yLen;
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
 
-            for (let x = xStart; x < xEnd; x++) {
-                for (let y = yStart; y < yEnd; y++) {
-                    level.tiles.set(x, y, {
-                        name: background.tile
-                    })
-                }
+        for (let x = xStart; x < xEnd; x++) {
+            for (let y = yStart; y < yEnd; y++) {
+                level.tiles.set(x, y, {
+                    name: background.tile
+                })
+            }
+        }
+    }
+
+    backgrounds.forEach(background => {
+        background.ranges.forEach((range) => {
+            if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1)
+            }
+            if (range.length === 3) {
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1)
+            }
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen)
             }
         })
     })
